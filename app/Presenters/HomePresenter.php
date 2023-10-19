96e31c4292fe6c4edc28eb7b583f60dfcc01c4de
Chrynn <filipmachala88@gmail.com>
2023-10-19 11:30:14 +0200

    MainTaskPresenter created

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
-use Nette\Application\UI\Presenter;
-
-final class HomePresenter extends Presenter
+class HomePresenter extends AbstractPresenter
 {
-
+	public function actionDefault(): void
+	{
+	}
 }
---
+++ app/Presenters/MainTaskPresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+class MainTaskPresenter extends AbstractPresenter
+{
+	public function actionDefault(): void
+	{
+	}
+}
