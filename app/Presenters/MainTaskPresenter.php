e5c50cf8745e2f58f5e37c1d5db85bf63eca3188
Chrynn <filipmachala88@gmail.com>
2023-10-20 18:52:26 +0200

    dataTable filtering by time added

--- app/Presenters/MainTaskPresenter.php
+++ app/Presenters/MainTaskPresenter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use App\Services\CronDatatable\CronDatatableService;
+use Nette\Application\UI\Form;
 
 class MainTaskPresenter extends AbstractPresenter
 {
@@ -21,34 +22,119 @@ class MainTaskPresenter extends AbstractPresenter
 		$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($dataTableSliceOffset);
 		$this->template->dataTableSliceOffset = $dataTableSliceOffset;
 		$this->template->dataTablePageIndicator = $dataTablePageIndicator;
+		$this->template->dataTableFilterByTimeStatus = false;
+		$this->template->dataTableTimeFilterTimeFrom = null;
+		$this->template->dataTableTimeFilterTimeTo = null;
 	}
 
-	public function handleShowNextRows(int $dataTableSliceOffset, int $dataTablePageIndicator): void
+	public function handleShowNextRows(
+		int $dataTableSliceOffset,
+		int $dataTablePageIndicator,
+		bool $dataTableFilterByTimeStatus,
+		int|null $dataTableTimeFilterTimeFrom,
+		int|null $dataTableTimeFilterTimeTo,
+	): void
 	{
-		$cronDataTableAllRowCount = $this->cronDatatableService->getCronDataTableAllRowCount();
-		$newDataTableSliceOffset = $dataTableSliceOffset + CronDatatableService::ROWS_PER_DATATABLE;
-		$newDataTablePageIndicator = $dataTablePageIndicator + 1;
-		$areNextRowsToShow = $newDataTableSliceOffset < $cronDataTableAllRowCount;
+		if (!$dataTableFilterByTimeStatus) {
+			$cronDataTableAllRowCount = $this->cronDatatableService->getCronDataTableAllRowCount();
+			$newDataTableSliceOffset = $dataTableSliceOffset + CronDatatableService::ROWS_PER_DATATABLE;
+			$newDataTablePageIndicator = $dataTablePageIndicator + 1;
+			$areNextRowsToShow = $newDataTableSliceOffset < $cronDataTableAllRowCount;
 
-		if ($areNextRowsToShow) {
-			$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($newDataTableSliceOffset);
-			$this->template->dataTableSliceOffset = $newDataTableSliceOffset;
-			$this->template->dataTablePageIndicator = $newDataTablePageIndicator;
+			if ($areNextRowsToShow) {
+				$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($newDataTableSliceOffset);
+				$this->template->dataTableSliceOffset = $newDataTableSliceOffset;
+				$this->template->dataTablePageIndicator = $newDataTablePageIndicator;
+			} else {
+				$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($dataTableSliceOffset);
+				$this->template->dataTableSliceOffset = $dataTableSliceOffset;
+				$this->template->dataTablePageIndicator = $dataTablePageIndicator;
+			}
 		} else {
-			$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($dataTableSliceOffset);
-			$this->template->dataTableSliceOffset = $dataTableSliceOffset;
-			$this->template->dataTablePageIndicator = $dataTablePageIndicator;
+			$cronDataTableAllRowCount = $this->cronDatatableService->getCronDatatableFilteredBytTimeCount($dataTableTimeFilterTimeFrom, $dataTableTimeFilterTimeTo);
+			$newDataTableSliceOffset = $dataTableSliceOffset + CronDatatableService::ROWS_PER_DATATABLE;
+			$newDataTablePageIndicator = $dataTablePageIndicator + 1;
+			$areNextRowsToShow = $newDataTableSliceOffset < $cronDataTableAllRowCount;
+
+			if ($areNextRowsToShow) {
+				$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableFilteredByTimeSliced($dataTableTimeFilterTimeFrom, $dataTableTimeFilterTimeTo, $newDataTableSliceOffset);
+				$this->template->dataTableSliceOffset = $newDataTableSliceOffset;
+				$this->template->dataTablePageIndicator = $newDataTablePageIndicator;
+			} else {
+				$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableFilteredByTimeSliced($dataTableTimeFilterTimeFrom, $dataTableTimeFilterTimeTo, $dataTableSliceOffset);
+				$this->template->dataTableSliceOffset = $dataTableSliceOffset;
+				$this->template->dataTablePageIndicator = $dataTablePageIndicator;
+			}
 		}
+
+		$this->template->dataTableFilterByTimeStatus = $dataTableFilterByTimeStatus;
+		$this->template->dataTableTimeFilterTimeFrom = $dataTableTimeFilterTimeFrom;
+		$this->template->dataTableTimeFilterTimeTo = $dataTableTimeFilterTimeTo;
 	}
 
-	public function handleShowLastRows(int $dataTableSliceOffset, int $dataTablePageIndicator): void
+	public function handleShowLastRows(
+		int $dataTableSliceOffset,
+		int $dataTablePageIndicator,
+		bool $dataTableFilterByTimeStatus,
+		int|null $dataTableTimeFilterTimeFrom,
+		int|null $dataTableTimeFilterTimeTo,
+	): void
 	{
 		if ($dataTableSliceOffset !== 0) {
 			$newDataTableSliceOffset = $dataTableSliceOffset - CronDatatableService::ROWS_PER_DATATABLE;
 			$newDataTablePageIndicator = $dataTablePageIndicator - 1;
-			$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($newDataTableSliceOffset);
-			$this->template->dataTableSliceOffset = $newDataTableSliceOffset;
-			$this->template->dataTablePageIndicator = $newDataTablePageIndicator;
+
+			if (!$dataTableFilterByTimeStatus) {
+				$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($newDataTableSliceOffset);
+				$this->template->dataTableSliceOffset = $newDataTableSliceOffset;
+				$this->template->dataTablePageIndicator = $newDataTablePageIndicator;
+			} else {
+				$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableFilteredByTimeSliced($dataTableTimeFilterTimeFrom, $dataTableTimeFilterTimeTo, $newDataTableSliceOffset);
+				$this->template->dataTableSliceOffset = $newDataTableSliceOffset;
+				$this->template->dataTablePageIndicator = $newDataTablePageIndicator;
+			}
 		}
+
+		$this->template->dataTableFilterByTimeStatus = $dataTableFilterByTimeStatus;
+		$this->template->dataTableTimeFilterTimeFrom = $dataTableTimeFilterTimeFrom;
+		$this->template->dataTableTimeFilterTimeTo = $dataTableTimeFilterTimeTo;
+	}
+
+	protected function createComponentFilterByTimeForm(): Form
+	{
+		$form = new Form();
+		$form->addInteger("timeFrom")
+			->setRequired('Please fill timeFrom input');
+		$form->addInteger("timeTo")
+			->setRequired('Please fill timeTo input');
+		$form->onSubmit[] = [$this, "filterByTime"];
+		return $form;
+	}
+
+	public function filterByTime(Form $form): void
+	{
+		$formValues = $form->getValues();
+		$valueTimeFrom = $formValues->timeFrom;
+		$valueTimeTo = $formValues->timeTo;
+		$dataTableSliceOffset = 0;
+		$dataTablePageIndicator = 1;
+		$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableFilteredByTimeSliced($valueTimeFrom, $valueTimeTo, $dataTableSliceOffset);
+		$this->template->dataTableSliceOffset = $dataTableSliceOffset;
+		$this->template->dataTablePageIndicator = $dataTablePageIndicator;
+		$this->template->dataTableFilterByTimeStatus = true;
+		$this->template->dataTableTimeFilterTimeFrom = $valueTimeFrom;
+		$this->template->dataTableTimeFilterTimeTo = $valueTimeTo;
+	}
+
+	public function handleResetFilter(): void
+	{
+		$dataTableSliceOffset = 0;
+		$dataTablePageIndicator = 1;
+		$this->template->dataTableDataArray = $this->cronDatatableService->getCronDatatableArraySliced($dataTableSliceOffset);
+		$this->template->dataTableSliceOffset = $dataTableSliceOffset;
+		$this->template->dataTablePageIndicator = $dataTablePageIndicator;
+		$this->template->dataTableFilterByTimeStatus = false;
+		$this->template->dataTableTimeFilterTimeFrom = null;
+		$this->template->dataTableTimeFilterTimeTo = null;
 	}
 }
--- app/Services/CronDatatable/CronDatatableService.php
+++ app/Services/CronDatatable/CronDatatableService.php
@@ -29,4 +29,25 @@ class CronDatatableService
 	{
 		return count($this->csvService->getArrayFromCsv(self::CRON_CSV_PATH));
 	}
+
+	public function getCronDatatableFilteredByTime(int $timeFrom, int $timeTo): array
+	{
+		$cronDataTableArray = $this->getCronDatatableArray();
+
+		return array_filter($cronDataTableArray, function ($row) use ($timeFrom, $timeTo) {
+			$averageTime = $row['Prumerny cas provedeni'];
+
+			return $averageTime >= $timeFrom && $averageTime <= $timeTo;
+		});
+	}
+
+	public function getCronDatatableFilteredBytTimeCount(int $timeFrom, int $timeTo): int
+	{
+		return count($this->getCronDatatableFilteredByTime($timeFrom, $timeTo));
+	}
+
+	public function getCronDatatableFilteredByTimeSliced(int $timeFrom, int $timeTo, int $dataTableSliceOffset): array
+	{
+		return array_slice($this->getCronDatatableFilteredByTime($timeFrom, $timeTo), $dataTableSliceOffset, self::ROWS_PER_DATATABLE, true);
+	}
 }
